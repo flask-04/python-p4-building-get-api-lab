7f085b5371bc00f0c12c4660be554c8433195a53
michael-korir <michaelkiptoo2000@gmail.com>
2024-01-26 10:38:58 +0300

    Build API

--- server/app.py
+++ server/app.py
@@ -20,19 +20,54 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakers= []
+    bakery= Bakery.query.all()
+    for bake in bakery:
+        bake_dict= {
+            "id": bake.id,
+            "name": bake.name,
+            "created_at": bake.created_at,
+            "updated_at": bake.updated_at
+        }
+        bakers.append(bake_dict)
+    # bake_dict= bakery.to_dict()
+    response= make_response(jsonify(bakers), 200, {"Content-Type": "application/json"})
+    return response
+    
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    baker_by_id= Bakery.query.filter(Bakery.id==id).first()
+    if not baker_by_id:
+        return make_response({"error":"No bakery with that ID"},404)
+    else:
+        response= make_response(jsonify(baker_by_id.to_dict()), 200)
+        return response
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    all_prices= []
+    prices= BakedGood.query.order_by(BakedGood.price.desc()).all()
+    for price in prices:
+        baked_good_dict= {
+            "id": price.id,
+            "name": price.name,
+            "price": price.price,
+        }
+        all_prices.append(baked_good_dict)
+    respose= make_response(jsonify(all_prices), 200)
+    return respose
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    # all_goods= []
+    goods= BakedGood.query.order_by(BakedGood.price.desc()).first()
+    # for good in goods:
+    good_dict= goods.to_dict()
+    # all_goods.append(good_dict)
+    response= make_response(jsonify(good_dict), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
